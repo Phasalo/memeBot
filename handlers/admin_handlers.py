bf548a5ec5f5536c46bbff47e3948529c5246bfe
CSSSensei <mihail.nikolenk0@yandex.ru>
2025-07-22 18:08:45 +0300

    Rename main_keyboard into inline_keyboards

--- handlers/admin_handlers.py
+++ handlers/admin_handlers.py
@@ -6,7 +6,7 @@ from DB import users_sqlite
 from DB.users_sqlite import Database
 from filters import filters
 from utils import format_string
-from keyboards import main_keyboard
+from keyboards import inline_keyboards as ikb
 
 router = Router()
 router.message.filter(filters.AdminUser())
@@ -14,7 +14,7 @@ router.message.filter(filters.AdminUser())
 
 @router.message(Command(commands='get_users'))  # /get_users
 async def cmd_get_users(message: Message):
-    await main_keyboard.get_users_by_page(message.from_user.id)
+    await ikb.get_users_by_page(message.from_user.id)
 
 
 @router.message(Command(commands='getcoms'))  # /getcoms
@@ -30,7 +30,6 @@ async def cmd_query(message: Message):
         amount = 5
     with Database() as db:
         for query in db.get_last_queries(int(amount)):
-            print(query.query_date)
             username = db.get_user(query.user_id).username
             query_time = query.query_date if query.query_date else '❓'
             line = f'<i>@{username if username else query.user_id}</i> — [{query_time}]: <blockquote>{format_string.format_string(query.query_text)}</blockquote>\n\n'
@@ -51,4 +50,4 @@ async def cmd_query(message: Message):
 @router.message(Command(commands='user_query'))  # /user_query
 async def cmd_user_query(message: Message):
     user_id_to_find = format_string.find_first_number(message.text)
-    await main_keyboard.user_query_by_page(message.from_user.id, user_id_to_find)
+    await ikb.user_query_by_page(message.from_user.id, user_id_to_find)
--- handlers/callbacks.py
+++ handlers/callbacks.py
@@ -1,6 +1,6 @@
 from aiogram import Router, F
 from aiogram.types import CallbackQuery
-from keyboards import main_keyboard
+from keyboards import inline_keyboards as ikb
 from handlers.callbacks_data import CutMessageCallBack
 
 router = Router()
@@ -12,8 +12,8 @@ async def cut_message_distributor(callback: CallbackQuery, callback_data: CutMes
     page = callback_data.page
     user_id = callback_data.user_id
     if action == 1:
-        await main_keyboard.get_users_by_page(callback.from_user.id, page, callback.message.message_id)
+        await ikb.get_users_by_page(callback.from_user.id, page, callback.message.message_id)
     elif action == 2:
-        await main_keyboard.user_query_by_page(callback.from_user.id, user_id, page, callback.message.message_id)
+        await ikb.user_query_by_page(callback.from_user.id, user_id, page, callback.message.message_id)
     elif action == -1:
         await callback.answer()
