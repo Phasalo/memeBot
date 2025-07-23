6bf116e4a823af64967e7313a47acbc5f6ed4783
CSSSensei <mihail.nikolenk0@yandex.ru>
2025-07-23 04:08:35 +0300

    Fix(models): set default query_count to 0 in User class

--- config/models.py
+++ config/models.py
@@ -12,7 +12,7 @@ class User:
     last_name: Optional[str] = None
     is_admin: bool = False
     registration_date: Optional[datetime] = None
-    query_count: Optional[int] = None
+    query_count: int = 0
 
     def full_name(self) -> str:
         """Возвращает полное имя пользователя"""
