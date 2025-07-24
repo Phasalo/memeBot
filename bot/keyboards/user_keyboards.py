006845ee46e03b06d5a4acf8999ddcf8305a23bf
MaxMavr <maxmavr2005@gmail.com>
2025-07-24 22:18:44 +0300

    Refactor: made it extensible get_query_count_emoji() Add: default icon

--- bot/keyboards/user_keyboards.py
+++ bot/keyboards/user_keyboards.py
@@ -1,9 +1,17 @@
-from aiogram.types import KeyboardButton, ReplyKeyboardMarkup, InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.utils.keyboard import ReplyKeyboardMarkup as KMarkup
+from aiogram.utils.keyboard import KeyboardButton as KButton
+from phrases import PHRASES_RU
 
-button_1: KeyboardButton = KeyboardButton(text='Кнопка 1')
-button_2: KeyboardButton = KeyboardButton(text='Кнопка 2')
 
-keyboard: ReplyKeyboardMarkup = ReplyKeyboardMarkup(
+def __make_placeholder_appeal() -> str:
+    return PHRASES_RU.placeholder_appeal
+
+
+button_1: KButton = KButton(text='Кнопка 1')
+button_2: KButton = KButton(text='Кнопка 2')
+
+keyboard: KMarkup = KMarkup(
     keyboard=[[button_1], [button_2]],
     resize_keyboard=True,
-    one_time_keyboard=False)
+    one_time_keyboard=False,
+    input_field_placeholder=__make_placeholder_appeal())
