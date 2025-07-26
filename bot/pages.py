258f9acd93ae3179a33b5eb22002f4883a274340
CSSSensei <mihail.nikolenk0@yandex.ru>
2025-07-26 16:25:53 +0300

    Refactor: delete deprecated function find_first_number(); move format_list functions to separate file

--- bot/pages.py
+++ bot/pages.py
@@ -5,7 +5,7 @@ from DB.tables.queries import QueriesTable
 from DB.tables.users import UsersTable
 from config import bot
 from config.const import USERS_PER_PAGE, QUERIES_PER_PAGE
-from utils import format_string
+from utils import format_list
 from bot.keyboards import inline_keyboards
 
 
@@ -13,7 +13,7 @@ async def get_users(user_id: int, page: int = 1, message_id: Union[int, None] = 
     with UsersTable() as users_db:
         users, pagination = users_db.get_all_users(page, USERS_PER_PAGE)
 
-        txt = format_string.format_user_list(users, pagination)
+        txt = format_list.format_user_list(users, pagination)
         reply_markup = inline_keyboards.page_keyboard(action=1, pagination=pagination)
 
         if message_id:
@@ -32,7 +32,7 @@ async def user_query(user_id: int, user_id_to_find: Union[int, None], page: int 
 
         user = users_db.get_user(user_id_to_find)
 
-        txt = format_string.format_queries_text(
+        txt = format_list.format_queries_text(
             queries=queries,
             username=user.username if user else None,
             user_id=user_id_to_find,
---
+++ utils/format_list.py
@@ -0,0 +1,69 @@
+from typing import List, Optional
+from DB.models import UserModel, QueryModel
+from phrases import PHRASES_RU
+from DB.models import Pagination
+from utils import format_string
+
+
+def format_user_list(users_info: List[UserModel], pagination: Pagination) -> str:
+    txt = [PHRASES_RU.title.users,
+           PHRASES_RU.replace('footnote.total', total=pagination.total_items)]
+
+    for user in users_info:
+        line_data = {
+            'username': f'@{user.username}' if user.username else PHRASES_RU.icon.not_username,
+            'user_id': str(user.user_id).ljust(12),
+            'query_stat': f'{format_string.get_query_count_emoji(user.query_count)} {user.query_count}',
+            'registration_date': user.registration_date.strftime('%d.%m.%Y')
+        }
+
+        user_line = PHRASES_RU.replace('template.user', **line_data)
+
+        if user.is_banned:
+            txt.append(f'<s>{user_line}</s>')
+        elif user.is_admin:
+            txt.append(f'<b>{user_line}</b>')
+        else:
+            txt.append(user_line)
+
+    if pagination.total_pages > 1:
+        txt.append(PHRASES_RU.icon.row_placeholder * (pagination.per_page - len(users_info)))
+
+    return ''.join(txt)
+
+
+def format_queries_text(
+        queries: List[QueryModel],
+        username: Optional[str] = None,
+        user_id: Optional[int] = None,
+        footnote_template: str = PHRASES_RU.footnote.user_query,
+        line_template: str = PHRASES_RU.template.user_query,
+        show_username: bool = False
+) -> str:
+    """
+    Форматирует список запросов в текстовое сообщение.
+
+    Args:
+        queries: Список объектов QueryModel
+        username: Имя пользователя (если есть)
+        user_id: ID пользователя (если username отсутствует)
+        footnote_template: Шаблон заголовка с {username} placeholder
+        line_template: Шаблон строки запроса с {time} и {query} placeholders
+        show_username: Показывать ли имя пользователя в каждой строке
+
+    Returns:
+        Отформатированная строка с историей запросов
+    """
+    username_display = f'@{username}' if username else (str(user_id) if user_id else PHRASES_RU.error.unknown)
+    txt = [PHRASES_RU.title.query,
+           footnote_template.format(username=username_display)]
+
+    for query in queries:
+        line_data = {
+            'time': query.query_date.strftime('%d.%m.%Y %H:%M:%S') if query.query_date else PHRASES_RU.error.unknown,
+            'query': query.query_text,
+            'username': f'@{query.user.username}' if show_username and query.user and query.user.username else ''
+        }
+        txt.append(line_template.format(**line_data))
+
+    return ''.join(txt)
