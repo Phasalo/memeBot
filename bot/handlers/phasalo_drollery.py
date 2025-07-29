dbbefd1e7ac74b9bbfff6b9f255c2c9f55d5aeca
MaxMavr <maxmavr2005@gmail.com>
2025-07-29 04:06:13 +0300

    CleanUp: optimize phasalo_drollery

--- bot/handlers/phasalo_drollery.py
+++ bot/handlers/phasalo_drollery.py
@@ -1,9 +1,9 @@
 from aiogram.types import Message
-from aiogram import Router
+from aiogram import Router, F
 
 router = Router()
 
 
-@router.message(lambda message: message.text.lower() == 'спасибо' or message.text.lower() == 'от души' or message.text.lower() == 'благодарю')
+@router.message(F.text.lower().in_(['спасибо', 'от души', 'благодарю']))
 async def _(message: Message):
     await message.answer_sticker(sticker='CAACAgEAAxkBAAEKShplAfTsN4pzL4pB_yuGKGksXz2oywACZQEAAnY3dj9hlcwZRAnaOjAE')
