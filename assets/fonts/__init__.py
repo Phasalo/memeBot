c269c1e7a1fac66c3d91ac855ccc161c18ff7b26
MaxMavr <maxmavr2005@gmail.com>
2025-08-18 20:01:42 +0300

    Add: fonts variables

---
+++ assets/fonts/__init__.py
@@ -0,0 +1,23 @@
+from config.const import FONTS_DIR
+
+FONT_IMPACT = str(FONTS_DIR / 'Impact.ttf')
+FONT_LOBSTER = str(FONTS_DIR / 'Lobster.ttf')
+
+FONT_ARIAL = str(FONTS_DIR / 'Arial.ttf')
+FONT_TIMES = str(FONTS_DIR / 'Times New Roman.ttf')
+
+FONT_ROBOTO_BOLD = str(FONTS_DIR / 'Roboto Bold.ttf')
+FONT_ROBOTO_REGULAR = str(FONTS_DIR / 'Roboto Regular.ttf')
+
+FONT_MYRIAD_BOLD = str(FONTS_DIR / 'Myriad Pro Bold.OTF')
+FONT_MYRIAD_COND_BOLD = str(FONTS_DIR / 'Myriad Pro Cond Bold.OTF')
+FONT_MYRIAD_COND_ITALIC = str(FONTS_DIR / 'Myriad Pro Cond Italic.OTF')
+
+LOREM_IPSUM = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' \
+              'sed do eiusmod tempor incididunt ut labore et dolore magna ' \
+              'aliqua. Ut enim ad minim veniam, quis nostrud exercitation ' \
+              'ullamco laboris nisi ut aliquip ex ea commodo consequat. ' \
+              'Duis aute irure dolor in reprehenderit in voluptate velit ' \
+              'esse cillum dolore eu fugiat nulla pariatur. Excepteur sint ' \
+              'occaecat cupidatat non proident, sunt in culpa qui officia ' \
+              'deserunt mollit anim id est laborum.'
