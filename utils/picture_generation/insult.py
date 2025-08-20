e77ae176b7e5c182fff275492f9a33e4b0c14596
MaxMavr <maxmavr2005@gmail.com>
2025-08-20 19:00:23 +0300

    Refactor: insult

--- utils/picture_generation/insult.py
+++ utils/picture_generation/insult.py
@@ -1,14 +1,17 @@
 import os
 from random import choice, shuffle
-from typing import Optional, Tuple
-
-from PIL import Image, ImageDraw, ImageFont
-from config.const import TEMP_DIR
-from utils.picture_generation.generation_utils.calculations import (unique_name,
-                                                                    fit_font_width,
-                                                                    text_height, size_minside)
-from utils.picture_generation.generation_utils.models import Text, Point, Stroke
-from assets.fonts import FONT_IMPACT
+from typing import Optional
+from PIL import Image, ImageDraw
+from utils.picture_generation.generation_utils.calculations import (font_to_width,
+                                                                    scale_to_min_side)
+from utils.picture_generation.generation_utils.fit_text import wrap_text
+from utils.picture_generation.generation_utils.models import (Point,
+                                                              Size,
+                                                              TextStyle,
+                                                              InsultStyle,
+                                                              DefaultTextStyle)
+from utils.picture_generation.generation_utils.processing import draw_multiline_text
+from utils.picture_generation.generation_utils.save import save_media
 from utils.string_formatter.word_declension import decline_word
 from moviepy import VideoFileClip, ImageClip, CompositeVideoClip
 
@@ -59,128 +62,127 @@ def random_town_resident() -> str:
 
 
 MIN_SIDE: int = 700
-MARGIN: int = 50
-DEFAULT_FONT_SIZE: int = 100
+MARGIN: int = 35
+MAX_FONT_SIZE: int = MIN_SIDE // 10
+MIN_FONT_SIZE: int = 40
 
 
 def create_insult_image(image_path: str,
-                        top_text: Optional[Text] = None,
-                        bottom_text: Optional[Text] = None,
-                        is_square: bool = True,
-                        with_cropped_top_text: bool = False,
-                        with_cropped_bottom_text: bool = False) -> str:
+                        top_text: Optional[str] = None,
+                        top_style: Optional[TextStyle] = None,
+                        bottom_text: Optional[str] = None,
+                        bottom_style: Optional[TextStyle] = None,
+                        image_style: InsultStyle = InsultStyle()) -> str:
     image = Image.open(image_path).convert('RGBA')
-    size = (MIN_SIDE, MIN_SIDE) if is_square else size_minside(image.size, MIN_SIDE)
-    image = image.resize(size, Image.LANCZOS)
+    size = Size(MIN_SIDE, MIN_SIDE) if image_style.is_square else scale_to_min_side(image.size, MIN_SIDE)
+    image = image.resize(size.as_tuple, Image.LANCZOS)
 
-    frame_image_path = _create_insult(size, top_text, bottom_text, with_cropped_top_text, with_cropped_bottom_text)
+    frame_image_path = _create_insult(size, top_text, top_style, bottom_text, bottom_style, image_style)
     frame_image = Image.open(frame_image_path).convert('RGBA')
 
     image = Image.alpha_composite(image, frame_image)
-
-    save_path = TEMP_DIR / unique_name(prefix='image_insult', extension='.png')
-    image.save(save_path, format='PNG')
     os.remove(frame_image_path)
-    return save_path
+
+    return save_media(image, prefix='insult')
 
 
 def create_insult_video(video_path: str,
-                        top_text: Optional[Text] = None,
-                        bottom_text: Optional[Text] = None,
-                        is_square: bool = True,
-                        with_cropped_top_text: bool = False,
-                        with_cropped_bottom_text: bool = False) -> str:
+                        top_text: Optional[str] = None,
+                        top_style: Optional[TextStyle] = None,
+                        bottom_text: Optional[str] = None,
+                        bottom_style: Optional[TextStyle] = None,
+                        image_style: InsultStyle = InsultStyle()) -> str:
     video = VideoFileClip(video_path)
-    size = (MIN_SIDE, MIN_SIDE) if is_square else size_minside(video.size, MIN_SIDE)
-    video = video.resized(size)
+    size = Size(MIN_SIDE, MIN_SIDE) if image_style.is_square else scale_to_min_side(video.size, MIN_SIDE)
+    video = video.resized(size.as_tuple)
 
-    frame_image_path = _create_insult(size, top_text, bottom_text, with_cropped_top_text, with_cropped_bottom_text)
+    frame_image_path = _create_insult(size, top_text, top_style, bottom_text, bottom_style, image_style)
     frame_image = ImageClip(frame_image_path).with_duration(video.duration)
 
     video = CompositeVideoClip([video, frame_image])
-
-    save_path = TEMP_DIR / unique_name(prefix='video_insult', extension='.mp4')
-    video.write_videofile(save_path)
     os.remove(frame_image_path)
-    return save_path
+
+    return save_media(video, prefix='insult')
 
 
-def _create_insult(image_size: Tuple[int, int],
-                   top_text: Optional[Text] = None,
-                   bottom_text: Optional[Text] = None,
-                   with_cropped_top_text: bool = False,
-                   with_cropped_bottom_text: bool = False) -> str:
+def _create_insult(image_size: Size,
+                   top_text: Optional[str] = None,
+                   top_style: Optional[TextStyle] = None,
+                   bottom_text: Optional[str] = None,
+                   bottom_style: Optional[TextStyle] = None,
+                   image_style: InsultStyle = InsultStyle()) -> str:
+    # Дефолтными значениями (Оскорбление города)
+    top_text = random_insult() if top_text is None else top_text
+    top_style = DefaultTextStyle() if top_style is None else top_style
+    bottom_text = random_town_resident() if bottom_text is None else bottom_text
+    bottom_style = DefaultTextStyle() if bottom_style is None else bottom_style
+
     image = Image.new("RGBA", image_size, (0, 0, 0, 0))
     canvas = ImageDraw.Draw(image, 'RGBA')
 
-    if not (top_text or bottom_text):
-        top_text = Text(text=random_insult(),
-                        font=FONT_IMPACT,
-                        color='#FFFFFF',
-                        stroke=Stroke(color='#000000', width=3))
+    # Размера шрифта style.font.point_size
+    width_text_block = image_size.width - (2 * MARGIN)
 
-        bottom_text = Text(text=random_town_resident(),
-                           font=FONT_IMPACT,
-                           color='#FFFFFF',
-                           stroke=Stroke(color='#000000', width=3))
+    top_style.font.point_size = MAX_FONT_SIZE
+    bottom_style.font.point_size = MAX_FONT_SIZE
 
-    width_text_container = image.width - (2 * MARGIN)
-    top_font_size = fit_font_width(top_text.text, width_text_container, top_text.font) \
-        if top_text else DEFAULT_FONT_SIZE
-    bottom_font_size = fit_font_width(bottom_text.text, width_text_container, bottom_text.font) \
-        if bottom_text else DEFAULT_FONT_SIZE
+    if top_text is not None:
+        top_style.font.point_size = font_to_width(top_text, width_text_block, top_style.font.path)
+    if bottom_text is not None:
+        bottom_style.font.point_size = font_to_width(bottom_text, width_text_block, bottom_style.font.path)
 
-    font_size = min(DEFAULT_FONT_SIZE, top_font_size, bottom_font_size)
+    font_size = min(top_style.font.point_size, bottom_style.font.point_size)
+    font_size = max(MIN_FONT_SIZE, min(MAX_FONT_SIZE, font_size))
 
-    top_font = ImageFont.truetype(top_text.font, font_size) if top_text else None
-    bottom_font = ImageFont.truetype(bottom_text.font, font_size) if bottom_text else None
+    top_style.font.point_size = bottom_style.font.point_size = font_size
 
-    center_x = image.width // 2
-    top_height = text_height(top_text.text, top_font) if top_text else 0
-    bottom_height = text_height(bottom_text.text, bottom_font) if bottom_text else 0
-
-    if with_cropped_top_text:
-        top_point = Point(center_x, -top_height // 2)
+    # Стартовые точки для текста
+    if image_style.crop_top_text:
+        top_point = Point(image_size.center_point.x, top_style.ascent // 2)
     else:
-        top_point = Point(center_x, MARGIN)
-    if with_cropped_bottom_text:
-        bottom_point = Point(center_x, image.height - bottom_height // 2)
+        top_point = Point(image_size.center_point.x, MARGIN + top_style.ascent)
+    if image_style.crop_bottom_text:
+        bottom_point = Point(image_size.center_point.x,
+                             image_size.height + bottom_style.line_height)
     else:
-        bottom_point = Point(center_x, image.height - MARGIN - bottom_height)
+        bottom_point = Point(image_size.center_point.x,
+                             image_size.height - MARGIN - bottom_style.descent)
+
+    height_text_block = image_size.height - (2 * MARGIN)
+    if top_text and bottom_text:
+        height_text_block //= 2
 
     if top_text:
-        canvas.text(
-            top_point.tuple,
-            top_text.text,
-            font=top_font,
-            fill=top_text.color,
-            stroke_width=top_text.stroke.width,
-            stroke_fill=top_text.stroke.color,
-            anchor="mt"
-        )
-    if bottom_text:
-        canvas.text(
-            bottom_point.tuple,
-            bottom_text.text,
-            font=bottom_font,
-            fill=bottom_text.color,
-            stroke_width=bottom_text.stroke.width,
-            stroke_fill=bottom_text.stroke.color,
-            anchor="mt"
-        )
+        top_text_list = wrap_text(top_text, top_style.font, Size(width_text_block, height_text_block))
+        draw_multiline_text(canvas, top_text_list, top_style, top_point)
 
-    save_path = TEMP_DIR / unique_name(prefix='raw_insult', extension='.png')
-    image.save(save_path, format='PNG')
-    return save_path
+    if bottom_text:
+        bottom_text_list = wrap_text(bottom_text, bottom_style.font, Size(width_text_block, height_text_block))
+        bottom_text_list.reverse()
+        draw_multiline_text(canvas, bottom_text_list, bottom_style, bottom_point,
+                            multiplier_shift=-1)
+
+    return save_media(image, prefix='raw_insult')
 
 
 if __name__ == '__main__':
-    from assets.fonts import FONT_IMPACT
+    from assets.fonts import FONT_IMPACT, LOREM_IPSUM, FONT_TIMES
 
-    create_insult_video(video_path=r'L:\maxim\PythonProjects\memeBot\temp\ast\fam.mp4',
-                        top_text=Text(text='Опа, а это видео', font=FONT_IMPACT),
-                        bottom_text=Text(text='Опа, а это видео', font=FONT_IMPACT))
+    # create_insult_video(video_path=r'L:\maxim\PythonProjects\memeBot\temp\ast\fam.mp4',
+    #                     top_text=Text(text='Опа, а это видео', font=FONT_IMPACT),
+    #                     bottom_text=Text(text='Опа, а это видео', font=FONT_IMPACT),
+    #                     is_square=True,
+    #                     with_cropped_top_text=False,
+    #                     with_cropped_bottom_text=True
+    #                     )
 
     create_insult_image(image_path=r'L:\maxim\PythonProjects\memeBot\temp\ast\dog2.jpg',
-                        top_text=Text(text='Опа, а это видео', font=FONT_IMPACT),
-                        bottom_text=Text(text='Опа, а это видео', font=FONT_IMPACT))
+                        top_text=Text(
+                            text='Я люблю собак, они дарят мне счастье, любовь и приятно',
+                            font=FONT_TIMES),
+                        bottom_text=Text(text='слизывают сперму',
+                                         font=FONT_TIMES),
+                        is_square=False,
+                        with_cropped_top_text=False,
+                        with_cropped_bottom_text=False
+                        )
