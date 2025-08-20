61782932beba3f95b484301b5545931d2cd1a3b8
MaxMavr <maxmavr2005@gmail.com>
2025-08-20 18:44:39 +0300

    Add: More fonts, Lorem ipsum with \n

--- assets/fonts/__init__.py
+++ assets/fonts/__init__.py
@@ -1,24 +1,69 @@
 from config.const import FONTS_DIR
 
-FONT_IMPACT = str(FONTS_DIR / 'Impact.ttf')
-FONT_LOBSTER = str(FONTS_DIR / 'Lobster.ttf')
-FONT_COMIC = str(FONTS_DIR / 'Comic Sans MS.ttf')
+# TODO Написать описание шрифтов для настроек https://vk.com/@shablony.for.comics-populyarnye-shrifty-v-pablikah
+#  FONT_AMERICAN (American Captain.ttf) —
+#  FONT_ARIAL (Arial.ttf) —
+#  FONT_ASTAKHOV (Astakhov First Simple.ttf) —
+#  FONT_ATTRACTIVE (Attractive Heavy.ttf) —
+#  FONT_COMIC (Comic Sans MS.ttf) —
+#  FONT_DOCKER (Docker One.ttf) —
+#  FONT_GAGALIN (Gagalin.otf) —
+#  FONT_GUERRILLA (Guerrilla Normal.otf) —
+#  FONT_HELIOSCOND (HeliosCond.otf) —
+#  FONT_IMPACT (Impact.ttf) —
+#  FONT_LAQONIC (Laqonic Unicase SemiBold Italic.otf) —
+#  FONT_LOBSTER (Lobster.ttf) —
+#  FONT_MOZER (Mozer SemiBold.otf) —
+#  FONT_MYRIAD_BOLD (Myriad Pro Bold.OTF) —
+#  FONT_MYRIAD_COND_BOLD (Myriad Pro Cond Bold.OTF) —
+#  FONT_MYRIAD_COND_ITALIC (Myriad Pro Cond Italic.OTF) —
+#  FONT_OBELIX (Obelix Pro.ttf) —
+#  FONT_ROBOTO_BOLD (Roboto Bold.ttf) —
+#  FONT_ROBOTO_REGULAR (Roboto Regular.ttf) —
+#  FONT_RODCHENKO (Rodchenko Condensed Bold.ttf) —
+#  FONT_TIMES (Times New Roman.ttf) —
+#  FONT_UNI (Uni Sans.ttf) —
 
+FONT_AMERICAN = str(FONTS_DIR / 'American Captain.ttf')
 FONT_ARIAL = str(FONTS_DIR / 'Arial.ttf')
-FONT_TIMES = str(FONTS_DIR / 'Times New Roman.ttf')
-
-FONT_ROBOTO_BOLD = str(FONTS_DIR / 'Roboto Bold.ttf')
-FONT_ROBOTO_REGULAR = str(FONTS_DIR / 'Roboto Regular.ttf')
-
+FONT_ASTAKHOV = str(FONTS_DIR / 'Astakhov First Simple.ttf')
+FONT_ATTRACTIVE = str(FONTS_DIR / 'Attractive Heavy.ttf')
+FONT_COMIC = str(FONTS_DIR / 'Comic Sans MS.ttf')
+FONT_DOCKER = str(FONTS_DIR / 'Docker One.ttf')
+FONT_GAGALIN = str(FONTS_DIR / 'Gagalin.otf')
+FONT_GUERRILLA = str(FONTS_DIR / 'Guerrilla Normal.otf')
+FONT_HELIOSCOND = str(FONTS_DIR / 'HeliosCond.otf')
+FONT_IMPACT = str(FONTS_DIR / 'Impact.ttf')
+FONT_LAQONIC = str(FONTS_DIR / 'Laqonic Unicase SemiBold Italic.otf')
+FONT_LOBSTER = str(FONTS_DIR / 'Lobster.ttf')
+FONT_MOZER = str(FONTS_DIR / 'Mozer SemiBold.otf')
 FONT_MYRIAD_BOLD = str(FONTS_DIR / 'Myriad Pro Bold.OTF')
 FONT_MYRIAD_COND_BOLD = str(FONTS_DIR / 'Myriad Pro Cond Bold.OTF')
 FONT_MYRIAD_COND_ITALIC = str(FONTS_DIR / 'Myriad Pro Cond Italic.OTF')
+FONT_OBELIX = str(FONTS_DIR / 'Obelix Pro.ttf')
+FONT_ROBOTO_BOLD = str(FONTS_DIR / 'Roboto Bold.ttf')
+FONT_ROBOTO_REGULAR = str(FONTS_DIR / 'Roboto Regular.ttf')
+FONT_RODCHENKO = str(FONTS_DIR / 'Rodchenko Condensed Bold.ttf')
+FONT_TIMES = str(FONTS_DIR / 'Times New Roman.ttf')
+FONT_UNI = str(FONTS_DIR / 'Uni Sans.ttf')
 
-LOREM_IPSUM = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, ' \
-              'sed do eiusmod tempor incididunt ut labore et dolore magna ' \
-              'aliqua. Ut enim ad minim veniam, quis nostrud exercitation ' \
+LOREM_IPSUM_N = '''Lorem ipsum dolor sit amet, 
+consectetur adipiscing elit, sed do eiusmod 
+tempor incididunt ut labore et dolore magna aliqua. 
+Ut enim ad minim veniam, quis nostrud exercitation 
+ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+Duis aute irure dolor in reprehenderit in voluptate velit 
+esse cillum dolore eu fugiat nulla pariatur. 
+Excepteur sint occaecat cupidatat non proident, 
+sunt in culpa qui officia deserunt mollit anim id est laborum.'''
+
+
+LOREM_IPSUM = 'Lorem ipsum dolor sit amet, ' \
+              'consectetur adipiscing elit, sed do eiusmod ' \
+              'tempor incididunt ut labore et dolore magna aliqua. ' \
+              'Ut enim ad minim veniam, quis nostrud exercitation ' \
               'ullamco laboris nisi ut aliquip ex ea commodo consequat. ' \
               'Duis aute irure dolor in reprehenderit in voluptate velit ' \
-              'esse cillum dolore eu fugiat nulla pariatur. Excepteur sint ' \
-              'occaecat cupidatat non proident, sunt in culpa qui officia ' \
-              'deserunt mollit anim id est laborum.'
+              'esse cillum dolore eu fugiat nulla pariatur. ' \
+              'Excepteur sint occaecat cupidatat non proident, ' \
+              'sunt in culpa qui officia deserunt mollit anim id est laborum.'
